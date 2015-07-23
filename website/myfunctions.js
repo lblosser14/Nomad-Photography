89980a667cf3e1684fe1bb6e42ff2e203930a997
lblosser14 <lblosser@live.com>
2015-07-23 16:37:54 -0600

    Photography Website
    This is the first website I have created.

---
+++ website/myfunctions.js
@@ -0,0 +1,13 @@
+	$(function(){
+    $('.fadein img:gt(0)').hide();
+    setInterval(function(){
+      $('.fadein :first-child').fadeOut()
+         .next('img').fadeIn()
+         .end().appendTo('.fadein');}, 
+      4000);
+});
+
+
+	
+	
+	
